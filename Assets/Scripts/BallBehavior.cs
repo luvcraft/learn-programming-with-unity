f6abc3975bfd1cd4e9b1f7ddc5b3961f464c056b
R Hunter Gough <huntybunz@yahoo.com>
2020-06-07 16:45:49 -0700

    changed MainBehavior to BallBehavior added lesson 2 scene

---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    private float x = 0;
+    private float y = 0;
+
+	// Start is called before the first frame update
+	private void Start()
+    {
+        
+    }
+
+	// Update is called once per frame
+	private void Update()
+    {
+        // start your code here!
+
+        x = 0;
+
+
+
+        // end your code here! Leave code under here alone.
+
+        UpdateBallPosition();
+    }
+
+    private void UpdateBallPosition()
+    {
+        transform.position = new Vector2(x, y);
+    }
+}
