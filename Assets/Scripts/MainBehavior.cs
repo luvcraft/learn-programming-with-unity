97555538ac735e224e99ea532cad991572bc8505
Amy Gough <manicstar@studiohunty.com>
2020-06-07 12:39:04 -0700

    initial commit

---
+++ Assets/Scripts/MainBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExtensionToolbox;
+using System.Collections.Specialized;
+
+public class MainBehavior : MonoBehaviour
+{
+    public Transform ball;
+
+    private float x = 0;
+    private float y = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // start your code here!
+
+
+
+
+
+
+
+
+
+        // end your code here! Leave code under here alone.
+
+        UpdateBallPosition();
+    }
+
+    void UpdateBallPosition()
+    {
+        ball.position = new Vector2(x, y);
+    }
+}
